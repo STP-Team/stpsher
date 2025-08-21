c63e6fe7632457081724e5b56ba63c09a491998e
Roman Chursanov <gh@chrsnv.ru>
2025-08-21 18:08:46 +0500

    feat: Меню графиков для руководителей

---
+++ tgbot/handlers/head/schedule/main.py
@@ -0,0 +1,34 @@
+import logging
+
+from aiogram import F, Router
+from aiogram.types import CallbackQuery
+
+from infrastructure.database.models import User
+from tgbot.filters.role import HeadFilter
+from tgbot.handlers.user.schedule.main import schedule_service
+from tgbot.keyboards.user.main import MainMenu
+from tgbot.keyboards.user.schedule.main import (
+    schedule_kb,
+)
+
+logger = logging.getLogger(__name__)
+
+head_schedule_router = Router()
+head_schedule_router.message.filter(F.chat.type == "private", HeadFilter())
+head_schedule_router.callback_query.filter(
+    F.message.chat.type == "private", HeadFilter()
+)
+
+
+@head_schedule_router.callback_query(MainMenu.filter(F.menu == "schedule"))
+async def schedule(callback: CallbackQuery, user: User):
+    """Главное меню расписаний"""
+    if not await schedule_service.check_user_auth(callback, user):
+        return
+
+    await callback.message.edit_text(
+        """📅 Меню графиков
+
+Используй меню для выбора действия""",
+        reply_markup=schedule_kb(),
+    )
