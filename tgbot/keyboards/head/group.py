26373aaee3c3f5606b74c7c5478beb7c2a2e3d94
Roman Chursanov <gh@chrsnv.ru>
2025-09-05 13:37:44 +0500

    feat: Просмотр расписания группы через меню менеджмента группы для руководителей

---
+++ tgbot/keyboards/head/group.py
@@ -0,0 +1,50 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.filters.callback_data import CallbackData
+
+from tgbot.keyboards.user.main import MainMenu
+
+
+class GroupManagementMenu(CallbackData, prefix="group_mgmt"):
+    menu: str
+
+
+def group_management_kb() -> InlineKeyboardMarkup:
+    """
+    Клавиатура управления группой.
+
+    :return: Объект встроенной клавиатуры для управления группой
+    """
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="📅 Расписание",
+                callback_data=GroupManagementMenu(menu="schedule").pack(),
+            ),
+            InlineKeyboardButton(
+                text="📊 KPI",
+                callback_data=GroupManagementMenu(menu="kpi").pack(),
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🏮 Игра",
+                callback_data=GroupManagementMenu(menu="game").pack(),
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="👥 Состав",
+                callback_data=GroupManagementMenu(menu="members").pack(),
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="↩️ Назад", callback_data=MainMenu(menu="main").pack()
+            ),
+        ],
+    ]
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=buttons,
+    )
+    return keyboard
