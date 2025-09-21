b623dcb7092b35a10a5c46b85a1089e9d54d6f16
Roman Chursanov <gh@chrsnv.ru>
2025-09-21 18:14:16 +0500

    fix(casino): Исправлен фильтр доступа к казино
    Closes #68
    
    Signed-off-by: Roman Chursanov <gh@chrsnv.ru>

--- tgbot/filters/casino.py
+++ tgbot/filters/casino.py
@@ -1,21 +1,19 @@
+from aiogram.dispatcher.event.bases import CancelHandler
 from aiogram.enums import ParseMode
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
-from marshmallow.fields import Boolean
 
 from infrastructure.database.models import Employee
 
 
 class IsCasinoAllowed(BaseFilter):
-    async def __call__(self, obj: Message, user: Employee, **kwargs) -> bool | Boolean:
-        if user is None:
-            return False
+    async def __call__(self, obj: Message, user: "Employee", **kwargs) -> bool:
+        if user is None or not user.is_casino_allowed:
+            await obj.answer(
+                """❌ Доступ к казино закрыт
 
-        await obj.answer(
-            """❌ Доступ к казино закрыт
-
-Если считаешь, что это ошибка - обратись к своему руководителю""",
-            show_alert=True,
-            parse_mode=ParseMode.HTML,
-        )
-        return user.is_casino_allowed
+Если считаешь, что это ошибка — обратись к своему руководителю""",
+                parse_mode=ParseMode.HTML,
+            )
+            raise CancelHandler()
+        return True
