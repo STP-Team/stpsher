62584803f87118e8f34a3607d981993b40899e97
Roman Chursanov <gh@chrsnv.ru>
2025-09-29 14:30:46 +0500

    refactor(states): Классификация состояний специалистов
    Signed-off-by: Roman Chursanov <gh@chrsnv.ru>

--- tgbot/misc/states/user/main.py
+++ tgbot/misc/states/user/main.py
@@ -2,22 +2,30 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class UserSG(StatesGroup):
+    # Меню
     menu = State()
-    schedule = State()
-    kpi = State()
-    kpi_requirements = State()
-    salary = State()
-    game = State()
-    game_shop = State()
-    game_shop_confirm = State()
-    game_shop_success = State()
-    game_inventory = State()
-    game_inventory_detail = State()
-    search = State()
     groups = State()
     settings = State()
+
+    schedule = State()
     schedule_my = State()
     schedule_my_detailed = State()
     schedule_group = State()
     schedule_duties = State()
     schedule_heads = State()
+    
+    kpi = State()
+    kpi_requirements = State()
+    salary = State()
+
+    game = State()
+    game_shop = State()
+    game_inventory = State()
+    game_inventory_detail = State()
+
+    search = State()
+
+    # Действия
+    game_shop_confirm = State()
+    game_shop_success = State()
+
