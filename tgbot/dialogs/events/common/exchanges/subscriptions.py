4b215eefe3edb9215ed4582f38019ebb3d8c3bbd
Roman Chursanov <roman@chrsnv.ru>
2025-11-08 18:44:42 +0500

    fix(exchanges): Исправление записи направлений в БД

--- tgbot/dialogs/events/common/exchanges/subscriptions.py
+++ tgbot/dialogs/events/common/exchanges/subscriptions.py
@@ -528,6 +528,12 @@ def _collect_subscription_data(dialog_manager: DialogManager, user: Employee) ->
     # Подразделения (новое поле target_divisions)
     divisions_data = dialog_manager.dialog_data.get("target_divisions")
     if divisions_data:
-        data["target_divisions"] = divisions_data
+        # Ensure proper UTF-8 encoding for Cyrillic characters
+        if isinstance(divisions_data, list):
+            data["target_divisions"] = [str(division) for division in divisions_data]
+        elif isinstance(divisions_data, str):
+            data["target_divisions"] = [divisions_data]
+        else:
+            data["target_divisions"] = divisions_data
 
     return data
